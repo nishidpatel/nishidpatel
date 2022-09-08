557eca2d1d43659e990fff4f2120eda24a5200e1
nishidpatel <nishidpatel71>
2022-09-08 13:05:36 +0530

    7_9_focus

--- hospital/src/conteinars/Auth/Auth.js
+++ hospital/src/conteinars/Auth/Auth.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 function Auth(props) {
 
     const [userType, setuserType] = useState('Login/sigup');
     const [reset, setreset] = useState(false)
+
+    const  nameRef = useRef();
+    const  emailRef = useRef();
+    const  passRef = useRef();
+
+    function handeling () {
+        passRef.current.style.border= '2px solid green'
+        passRef.current.focus()
+        console.log( passRef.current.value);
+        console.log(emailRef.current.value);
+    }
+
 
     return (
         <section id="appointment" className="appointment">
@@ -38,7 +50,7 @@ function Auth(props) {
                                     null
                                     :
                                     <div className="row">
-                                        <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                        <input ref={nameRef} type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                                         <div className="validate" />
                                     </div>
 
@@ -48,7 +60,7 @@ function Auth(props) {
                     </div>
                     <div className="row">
                         <div className="col-md-4 form-group mt-3 mt-md-0">
-                            <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                            <input ref={emailRef} type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
                             <div className="validate" />
                         </div>
 
@@ -62,7 +74,7 @@ function Auth(props) {
                             
                             <div className="row">
                                 <div className="col-md-4 form-group mt-3 mt-md-0">
-                                    <input type="tel" className="form-control" name="pssword" id="pssword" placeholder="Your pssword" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                    <input ref={passRef} type="tel" className="form-control" name="pssword" id="pssword" placeholder="Your pssword" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                                     <div className="validate" />
                                 </div>
                             </div>
@@ -77,7 +89,7 @@ function Auth(props) {
                         :
 
                         userType === 'Login'?
-                            <div className="text-center"><button type="submit">Login</button></div>
+                            <div className="text-center"><button type="submit" onClick={() => handeling()}>Login</button></div>
                             :
                             <div className="text-center"><button type="submit">Sigup</button></div>
 
