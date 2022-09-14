ac1f35aa2f06cd49a4c55a2ec25d830f6862fe7b
nishidpatel <nishidpatel71>
2022-09-14 16:31:12 +0530

    14_9_namevalidasion

--- hospital/src/conteinars/Auth/Auth.js
+++ hospital/src/conteinars/Auth/Auth.js
@@ -1,138 +1,190 @@
 import React, { useRef, useState } from 'react';
+import * as yup from 'yup';
+import { Form, Formik, useFormik } from 'formik';
 
 function Auth(props) {
-
-    const [userType, setuserType] = useState('Login/sigup');
+    const [userType, setuserType] = useState('Login');
     const [reset, setreset] = useState(false)
 
-    const  nameRef = useRef();
-    const  emailRef = useRef();
-    const  passRef = useRef();
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passRef = useRef();
 
-    function handeling () {
-        passRef.current.style.border= '2px solid green'
-        passRef.current.focus()
-        console.log( passRef.current.value);
+    function handel() {
+        passRef.current.focus();
+        passRef.current.style.border = "2px solid yellow"
         console.log(emailRef.current.value);
-    }
+        console.log(passRef.current.value);
 
+    }
     function handelon() {
         nameRef.current.focus();
         nameRef.current.style.border = '2px solid blue'
     }
 
-    let authschema;
-    let schema = yup.object().shape(authschema);
-    if (userType === 'singup' && reset === false) {
+    let authschema, init;
+
+    if (userType === 'Singup' && reset === false) {
         authschema = {
             name: yup.string().required("please Enter your name"),
             email: yup.string().required("please Enter valid email").email("please Enter email"),
             pssword: yup.string().required("Enter your pssword").min(8)
         }
+        
+        init = {
+            name: '',
+            email: '',
+            pssword: '',
+        }
 
-    } else if (userType === 'login' && reset === false) {
+    } else if (userType === 'Login' && reset === false) {
         authschema = {
             email: yup.string().required("please Enter valid email").email("please Enter email"),
             pssword: yup.string().required("Enter your pssword").min(8),
+        }
+
+        init = {
+            email: '',
+            pssword: '',
         }
     } else if (reset === true) {
         authschema = {
             email: yup.string().required("please Enter valid email").email("please Enter email"),
         }
+
+        init = {
+            email: '',
+        }
     }
 
+    console.log(authschema, init);
+    let schema = yup.object().shape(authschema);
+
+    const formik = useFormik({
+        
+        initialValues: init,
+        validationSchema: schema,
+        onSubmit: values => {
+
+            console.log(values);
+
+        },
+    });
+
+    const { handleChange, errors, handleSubmit } = formik;
 
     return (
         <section id="appointment" className="appointment">
             <div className="container">
                 <div className="section-title">
-
                     {
-
-                        reset === true?
+                        reset === true ?
                             <h2>Reset password</h2>
                             :
 
-                            userType === 'Login'?
+                            userType === 'Login' ?
                                 <h2>Login</h2>
                                 :
-                                <h2>signup</h2>
-
+                                <h2>Singup</h2>
                     }
 
 
                 </div>
-                <div action method="post" role="form" className="php-email-form">
+                <Formik value={formik}>
+                    <Form className="php-email-form">
 
-                    <div className="col-md-4 form-group">
-
-                        {
-
-                            reset === true?
-                                null
-                                :
-                                userType === 'Login'?
+                        <div className="col-md-4 form-group">
+                            {
+                                reset === true ?
                                     null
                                     :
-                                    <div className="row">
-                                        <input ref={nameRef} type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                    userType === 'Login' ?
+                                        null
+                                        :
+                                        <div className="row">
+
+                                            <input
+                                                type="text"
+                                                name="name"
+                                                className="form-control"
+                                                id="name" placeholder="Your Name"
+                                                data-rule="minlen:4"
+                                                data-msg="Please enter at least 4 chars"
+                                                onChange={handleChange}/>
+
+                                            <p>{errors.name}</p>
+                                            <div className="validate" />
+                                        </div>
+                            }
+
+
+                        </div>
+                        <div className="row">
+                            <div className="col-md-4 form-group mt-3 mt-md-0">
+
+                                <input 
+                                    ref={emailRef}
+                                    type="email"
+                                    className="form-control"
+                                    name="email" id="email"
+                                    placeholder="Your Email"
+                                    onChange={handleChange}/>
+
+                                <p>{errors.email}</p>
+                                <div className="validate" />
+                            </div>
+
+                        </div>
+                        {
+                            reset === true ?
+                                null
+                                :
+                                <div className="row">
+                                    <div className="col-md-4 form-group mt-3 mt-md-0">
+
+                                        <input 
+                                            ref={passRef}
+                                            type="tel"
+                                            className="form-control"
+                                            name="pssword" id="pssword"
+                                            placeholder="Your pssword"
+                                            data-rule="minlen:4"
+                                            data-msg="Please enter at least 4 chars"
+                                            onChange={handleChange} />
+
+                                        <p>{errors.pssword}</p>
                                         <div className="validate" />
                                     </div>
-
+                                </div>
                         }
 
 
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4 form-group mt-3 mt-md-0">
-                            <input ref={emailRef} type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                            <div className="validate" />
-                        </div>
-
-                    </div>
-
-                    {
-
-                        reset === true?
-                            null
-                            :
-                            
-                            <div className="row">
-                                <div className="col-md-4 form-group mt-3 mt-md-0">
-                                    <input ref={passRef} type="tel" className="form-control" name="pssword" id="pssword" placeholder="Your pssword" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                    <div className="validate" />
-                                </div>
-                            </div>
-
-                    }
+                        {
+                            reset === true ?
+                                <div className="text-center"><button type="submit">submit</button></div>
+                                :
+                                userType === 'Login' ?
+                                    <div className="text-center"><button type="submit" >Login</button></div>
+                                    :
+                                    <div className="text-center"><button type="submit"  >sigup</button></div>
+                        }
 
 
-                    {
+                    </Form>
+                </Formik>
 
-                        reset === true?
-                        <div className="text-center"><button type="submit">submit</button></div>
+                {
+                    userType === 'Login' ?
+                        <div>Create a new Account1 <button onClick={() => { setreset(false); setuserType('Singup') }}>Signup</button></div>
                         :
+                        <div>Already have Account1 <button onClick={() => { setreset(false); setuserType('Login') }}>Login</button></div>
 
-                        userType === 'Login'?
-                            <div className="text-center"><button type="submit" onClick={() => handeling()}>Login</button></div>
-                            :
-                            <div className="text-center"><button type="submit">Sigup</button></div>
+                }
 
-                    }
+                <span>Forget password <button onClick={() => setreset(true)}>Click Here</button></span>
 
-
-                    {
-
-                        userType === 'Login'?
-                            <div>Create a new Account1 <button onClick={() => {setreset(false); setuserType('Singup')}}>Signup</button></div>
-                            :
-                            <div>Already have Account1 <button onClick={() => {setreset(false); setuserType('Login')}}>Login</button></div>
-
-                    }
-                    
-                    <span>Forget password <button onClick={() => setreset(true)}>Click Here</button></span>
-                </div>
             </div>
+
         </section>
 
 
